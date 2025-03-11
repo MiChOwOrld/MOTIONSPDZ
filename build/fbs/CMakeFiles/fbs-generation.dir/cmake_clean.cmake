8fa3e1fb2fccc2c60dc796f9cf28ddbb45a361b1
MiChOwOrld <giorgoss.michopouloss@gmail.com>
2025-03-11 21:38:36 +0200

    new
    changes

---
+++ build/fbs/CMakeFiles/fbs-generation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/micho/MOTION1/src/motioncore/communication/fbs_headers/garbled_circuit_message_generated.h"
+  "/home/micho/MOTION1/src/motioncore/communication/fbs_headers/hello_message_generated.h"
+  "/home/micho/MOTION1/src/motioncore/communication/fbs_headers/message_generated.h"
+  "CMakeFiles/fbs-generation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/fbs-generation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/micho/libraries/googletest/googletest
+# Build directory: /home/micho/MOTION1/build/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
